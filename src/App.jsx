162167c1b9a5ba09597727054caf72f9f3ea08d5
TatianaAll <tatiana.allery@orange.fr>
2026-01-09 15:02:39 +0100

    feat: add bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
 // import { useState } from 'react';
 import datasPoke from "./assets/data.json";
-import "./pokemon.css";
-import "./App.css";
-import "./Normalize.css";
+import "./css/style-pokemons.css";
+import "./css/normalize.css";
 
 function App() {
-  const pokemons = datasPoke.map((pokemon) => {
-    return (
-      <article>
-        <figure id={pokemon.id} key={pokemon.id}>
-          <picture>
-            <img src={pokemon.image} alt={`Image ${pokemon.name}`} />
-          </picture>
-          <figcaption>
-            <span className="types">{pokemon.apiTypes[0].name}</span>
-            <h2>{pokemon.name}</h2>
-            <ol>
-              <li>Points de vie : {pokemon.stats.HP}</li>
-              <li>Attaque : {pokemon.stats.attack}</li>
-              <li>Défense : {pokemon.stats.defense}</li>
-              <li>Attaque spécial : {pokemon.stats.special_attack}</li>
-              <li>Vitesse : {pokemon.stats.speed}</li>
-            </ol>
-          </figcaption>
-        </figure>
-      </article>
-    );
-  });
   return (
     <>
-    <div className="container-pokemons">
-      {pokemons}
-    </div>
-      
+      <main>
+        {datasPoke.map((pokemon) => (
+          <article id={pokemon.id} key={pokemon.id} className={pokemon.apiTypes[0].name}>
+            <figure>
+              <picture>
+                <img src={pokemon.image} alt={`Image ${pokemon.name}`} />
+              </picture>
+              <figcaption>
+                <span className="types">{pokemon.apiTypes[0].name}</span>
+                <h2>{pokemon.name}</h2>
+                <ol>
+                  <li>Points de vie : {pokemon.stats.HP}</li>
+                  <li>Attaque : {pokemon.stats.attack}</li>
+                  <li>Défense : {pokemon.stats.defense}</li>
+                  <li>Attaque spécial : {pokemon.stats.special_attack}</li>
+                  <li>Vitesse : {pokemon.stats.speed}</li>
+                </ol>
+              </figcaption>
+            </figure>
+          </article>
+        ))}
+      </main>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
 import App from "./App.jsx";
+import logo from "../public/logo.png";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-
+  <StrictMode>
+    <h1 style= {{ textAlign: "center", width: "30%" , margin: "auto" }}>
+      <img src={logo} alt="Logo" style={{ width: "100%"}} />
+    </h1>
+    <App />
+  </StrictMode>
 );
